3fd4afaee7dbdfe03f4aa787b1b922c47841016c
Mike Wekesa <mikewamalwa92@gmail.com>
2025-09-16 18:07:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,47 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+st.title("CORD-19 Data Explorer")
+st.write("Simple exploration of COVID-19 research papers (metadata.csv)")
+
+# Load data
+@st.cache_data
+def load_data():
+    return pd.read_csv("data/metadata.csv")
+
+df = load_data()
+df['publish_time'] = pd.to_datetime(df['publish_time'], errors='coerce')
+df['year'] = df['publish_time'].dt.year
+
+# Sidebar filter
+year_range = st.slider("Select year range", 2015, 2023, (2019, 2021))
+filtered = df[(df['year'] >= year_range[0]) & (df['year'] <= year_range[1])]
+
+# Publications over time
+st.subheader("Publications by Year")
+year_counts = filtered['year'].value_counts().sort_index()
+fig, ax = plt.subplots()
+ax.bar(year_counts.index, year_counts.values, color="skyblue")
+st.pyplot(fig)
+
+# Top journals
+st.subheader("Top Journals")
+top_journals = filtered['journal'].value_counts().head(5)
+st.bar_chart(top_journals)
+
+# Word cloud
+st.subheader("Word Cloud of Titles")
+text = " ".join(title for title in filtered['title'].dropna())
+if text:
+    wordcloud = WordCloud(width=800, height=400, background_color="white").generate(text)
+    fig, ax = plt.subplots()
+    ax.imshow(wordcloud, interpolation='bilinear')
+    ax.axis("off")
+    st.pyplot(fig)
+
+# Sample data
+st.subheader("Sample Data")
+st.write(filtered.head(10))
 
